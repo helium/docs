576ba2bf4aa088f79001ecd6db69e5b5c39781ed
Kent Wiliams <k3nt00@gmail.com>
2020-10-01 13:38:48 -0700

    changed site url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://helium-docs.herokuapp.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   favicon: 'img/favicon.ico',
