b93490134588c1fdcbc4ad39fa0e90085c6efbae
Kent Wiliams <k3nt00@gmail.com>
2020-10-01 14:58:05 -0700

    remove blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,6 @@ module.exports = {
           label: 'Docs',
           position: 'left',
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
