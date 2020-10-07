8446e4523c2edbfd2285621676f6bdb6f4f6e45a
Kent Wiliams <k3nt00@gmail.com>
2020-10-06 18:18:22 -0700

    initial skeleton

--- sidebars.js
+++ sidebars.js
@@ -3,9 +3,39 @@ module.exports = {
     'home',
     {
       type: 'category',
-      label: 'Console',
-      items: ['console/introduction'],
+      label: 'Blockchain',
+      items: ['blockchain/blockchain'],
       collapsed: true,
-    }
+    },
+    {
+      type: 'category',
+      label: 'Miners',
+      items: ['miners/miners'],
+      collapsed: true,
+    },
+    {
+      type: 'category',
+      label: 'Devices',
+      items: ['devices/devices', 'devices/ready-to-use/ready-to-use', 'devices/development/development'],
+      collapsed: true,
+    },
+    {
+      type: 'category',
+      label: 'Console',
+      items: ['console/console'],
+      collapsed: true,
+    },
+    {
+      type: 'category',
+      label: 'Network Servers',
+      items: ['network-servers/network-servers'],
+      collapsed: true,
+    },
+    {
+      type: 'category',
+      label: 'Community and Governance',
+      items: ['community-governance/community-governance'],
+      collapsed: true,
+    },
   ],
 };
