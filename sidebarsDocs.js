323792453e6c42b268812fbb43203321472f3ffb
Kent Williams <k3nt00@gmail.com>
2020-12-15 10:23:14 -0800

    add device content

--- sidebarsDocs.js
+++ sidebarsDocs.js
@@ -53,7 +53,7 @@ module.exports = {
     {
       type: 'category',
       label: 'Console',
-      items: ['use-the-network/console/quickstart', 'use-the-network/console/users'],
+      items: ['use-the-network/console/quickstart', 'use-the-network/console/users', 'use-the-network/console/adding-devices'],
       collapsed: false,
     }
   ],
