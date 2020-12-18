2644f9698e7e4815ec41505df0bd65ab0e296504
Kent Williams <k3nt00@gmail.com>
2020-12-17 17:18:59 -0800

    add migrating section

--- sidebarsDocs.js
+++ sidebarsDocs.js
@@ -53,7 +53,7 @@ module.exports = {
     {
       type: 'category',
       label: 'Console',
-      items: ['use-the-network/console/quickstart', 'use-the-network/console/users', 'use-the-network/console/adding-devices', 'use-the-network/console/labels', 'use-the-network/console/debug', 'use-the-network/console/functions', 'use-the-network/console/integrations/integrations'],
+      items: ['use-the-network/console/quickstart', 'use-the-network/console/users', 'use-the-network/console/adding-devices', 'use-the-network/console/migrating/migrating', 'use-the-network/console/labels', 'use-the-network/console/debug', 'use-the-network/console/functions', 'use-the-network/console/integrations/integrations'],
       collapsed: false,
     }
   ],
