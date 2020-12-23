ac3afb6e431d74e8b53fa3e5f9282dd96d091d0e
Kent Williams <k3nt00@gmail.com>
2020-12-22 17:24:44 -0800

    add oracle prices

--- sidebarsAPI.js
+++ sidebarsAPI.js
@@ -4,7 +4,7 @@ module.exports = {
         {
       type: 'category',
       label: 'Blockchain',
-      items: ['blockchain/introduction', 'blockchain/stats', 'blockchain/blocks', 'blockchain/accounts', 'blockchain/hotspots', 'blockchain/cities', 'blockchain/transactions', 'blockchain/pending-transactions'],
+      items: ['blockchain/introduction', 'blockchain/stats', 'blockchain/blocks', 'blockchain/accounts', 'blockchain/hotspots', 'blockchain/cities', 'blockchain/transactions', 'blockchain/pending-transactions', 'blockchain/oracle-prices'],
       collapsed: true,
     },
     'console',
