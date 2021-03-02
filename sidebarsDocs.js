157bd872b82aa24960e37b94c59432c208883581
Leroy <levinle56@gmail.com>
2021-03-01 20:40:28 -0600

    add RAK WisBlock Arduino doc, fix broken external links, add link to Integrations

--- sidebarsDocs.js
+++ sidebarsDocs.js
@@ -212,7 +212,7 @@ module.exports = {
      {
        type: 'category',
        label: 'Firmware', 
-       items: ['use-the-network/devices/development/rak-wisblock-starter/platformio'],
+       items: ['use-the-network/devices/development/rak-wisblock-starter/arduino', 'use-the-network/devices/development/rak-wisblock-starter/platformio'],
        collapsed: false,
      },
    ],
