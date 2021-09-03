188e28748e977cab157c2d91a403af0bf501e010
jkowalcz <git@kowalczyk.org>
2021-09-03 15:10:40 -0400

    Move to it's own file and intergrate it like Testnet

--- sidebarsDocs.js
+++ sidebarsDocs.js
@@ -1086,6 +1086,7 @@ module.exports = {
      items: [
        'mine-hnt/validators/mainnet/validators-mainnet-deployment-guide',
        'mine-hnt/validators/mainnet/validators-mainnet-release',
+	   'mine-hnt/validators/mainnet/validators-mainnet-wallet',
      ],
      collapsed: false,
    },
