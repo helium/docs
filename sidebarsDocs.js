ad0738ee71243390748411cb7e354355d4bc757f
Kent Williams <k3nt00@gmail.com>
2022-03-14 10:41:48 -0700

    add initial roaming services section

--- sidebarsDocs.js
+++ sidebarsDocs.js
@@ -47,6 +47,7 @@ module.exports = {
           'use-the-network/use-the-network',
           'use-the-network/devices/devices',
           'use-the-network/console/console',
+          'use-the-network/roaming/roaming',
           'use-the-network/run-a-network-server/run-a-network-server',
           'use-the-network/setup-a-packet-forwarder',
           'use-the-network/community-projects/community-projects',
