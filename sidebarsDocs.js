558c2a29c33ddae2a388c12a0e0825bab720b020
Jeremy Ellis <keyfreemusic@gmail.com>
2022-06-08 09:53:09 -0700

    Lora vision shield v2 (#896)
    * moving this image to the lora-vision-shield folder
    
    * upload lora-vision-shield-images
    
    * moving file to lora-vision-shield folder
    
    * uploading lora-vision-shield folder for docs
    
    * updated file to include lora-vision-shield
    
    * Update sidebarsDocs.js
    
    * Update arduino.mdx
    
    * Update sidebarsDocs.js
    
    * fixed issue on png file
    
    * Update arduino.mdx
    
    * upload the portenta-bootloader-image
    
    * updated image for bootloader
    
    * Update lora-vision-shield.mdx
    
    * Update arduino.mdx
    
    * Update arduino.mdx
    
    * Update arduino.mdx
    
    * Add files via upload
    
    * Update arduino.mdx
    
    * Update arduino.mdx
    
    * Update arduino.mdx
    
    * Update arduino.mdx
    
    * Update arduino.mdx
    
    * Update arduino.mdx
    
    * Update arduino.mdx
    
    * Update arduino.mdx
    
    * Update arduino.mdx

--- sidebarsDocs.js
+++ sidebarsDocs.js
@@ -297,7 +297,7 @@ module.exports = {
       label: 'Arduino',
       items: [
         'use-the-network/devices/development/arduino/mkr-wan-1310',
-        'use-the-network/devices/development/arduino/lora-vision-shield'
+        'use-the-network/devices/development/arduino/lora-vision-shield/lora-vision-shield'
       ],
       collapsed: false,
     },
@@ -1197,6 +1197,19 @@ module.exports = {
       type: 'category',
       label: 'Guides',
       items: ['use-the-network/devices/development/adafruit/adafruit-feather-m0-rfm95/adafruitio'],
+      collapsed: false,
+    },
+  ],  
+  loravisionshieldv2: [
+   {
+      type: 'link',
+      label: '<- Arduino LoRa Vision Shield',
+      href: '/use-the-network/devices/development/arduino/lora-vision-shield'
+    },
+    {
+      type: 'category',
+      label: 'Firmware',
+      items: ['use-the-network/devices/development/arduino/lora-vision-shield/arduino'],
       collapsed: false,
     },
   ],
