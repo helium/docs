598fe2fa5edb1f79ec823ec86cab7983ab7af37f
Mark Angelo Nambatac <nambatac80@gmail.com>
2022-06-08 16:09:19 +0800

    Added RAK7204

--- sidebarsDocs.js
+++ sidebarsDocs.js
@@ -935,6 +935,23 @@ module.exports = {
       collapsed: false,
     },
   ],
+  rakwireless: [
+    {
+       type: 'link',
+       label: '<- Back',
+       href: '/use-the-network/devices/ready-to-use'
+     },
+     {
+       type: 'category',
+       label: 'RAKwireless',
+       items: [
+         'use-the-network/devices/ready-to-use/rakwireless/rak7204',
+       ],
+ 
+       collapsed: false,
+     },
+   ],
+
   seeedstudiortu: [
    {
       type: 'link',
