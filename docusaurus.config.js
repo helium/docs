506043fe3adb27e208208783df968ba80c5a3849
Sam Gutentag <developer@samgutentag.com>
2023-01-18 13:38:29 -0800

    updating config to latest docusaurus templates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,103 +1,51 @@
-module.exports = {
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github')
+const darkCodeTheme = require('prism-react-renderer/themes/dracula')
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
   title: 'Helium Documentation',
   tagline: 'Documentation for the Helium Network',
   url: 'https://docs.helium.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
   organizationName: 'helium', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
-  themeConfig: {
-    docs: {
-      sidebar: {
-        hideable: true,
-      },
-    },
-    // hideableSidebar: true,
-    algolia: {
-      appId: 'XC618T80WL',
-      apiKey: '7d9c9bfed6c45d91cb148ac5a365079d',
-      indexName: 'helium',
-      contextualSearch: false,
-    },
-    colorMode: {
-      // "light" | "dark"
-      defaultMode: 'light',
 
-      // Hides the switch in the navbar
-      // Useful if you want to support a single color mode
-      disableSwitch: true,
-
-      // Should we use the prefers-color-scheme media-query,
-      // using user system preferences, instead of the hardcoded defaultMode
-      respectPrefersColorScheme: false,
-    },
-    navbar: {
-      title: '',
-      style: 'primary',
-      logo: {
-        alt: 'Helium Logo',
-        src: 'img/icons/logoblack.svg',
-        srcDark: 'img/logowhite.svg',
-      },
-      items: [
-        {
-          to: '/use-the-network/console',
-          label: 'Try the New Console Today',
-          position: 'right',
-          activeBaseRegex: `docs`,
-          className: 'announcementnav',
-        },
-        {
-          to: '/',
-          label: 'Docs',
-          position: 'right',
-          activeBaseRegex: `docs`,
-        },
-        {
-          to: 'api',
-          label: 'API',
-          position: 'right',
-          activeBaseRegex: `api`,
-        },
-        {
-          href: 'https://github.com/helium',
-          position: 'right',
-          label: 'Github',
-        },
-        {
-          href: 'http://whitepaper.helium.com',
-          label: 'Whitepaper (2018)',
-          position: 'right',
-        },
-
-        {
-          href: 'https://explorer.helium.com',
-          label: 'Explorer',
-          position: 'right',
-        },
-        {
-          href: 'https://network.helium.com/coverage',
-          label: 'Coverage Map',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      copyright: `Copyright © ${new Date().getFullYear()} Helium Foundation`,
-    },
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
   },
+
   presets: [
     [
-      '@docusaurus/preset-classic',
-      {
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
         docs: {
           breadcrumbs: false,
           sidebarPath: require.resolve('./sidebarsDocs.js'),
           routeBasePath: '/',
           // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
           editUrl: 'https://github.com/helium/docs/edit/master',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         googleAnalytics: {
           trackingID: 'UA-52432858-10',
@@ -105,12 +53,84 @@ module.exports = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-      },
+      }),
     ],
   ],
-  plugins: [],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      algolia: {
+        appId: 'XC618T80WL',
+        apiKey: '7d9c9bfed6c45d91cb148ac5a365079d',
+        indexName: 'helium',
+        contextualSearch: false,
+      },
+      navbar: {
+        title: '',
+        style: 'primary',
+        logo: {
+          alt: 'Helium Logo',
+          src: 'img/icons/logoblack.svg',
+          srcDark: 'img/logowhite.svg',
+        },
+        items: [
+          {
+            to: '/use-the-network/console',
+            label: 'Try the New Console Today',
+            position: 'right',
+            activeBaseRegex: `docs`,
+            className: 'announcementnav',
+          },
+          {
+            to: '/',
+            label: 'Docs',
+            position: 'right',
+            activeBaseRegex: `docs`,
+          },
+          {
+            to: 'api',
+            label: 'API',
+            position: 'right',
+            activeBaseRegex: `api`,
+          },
+          {
+            href: 'https://github.com/helium',
+            position: 'right',
+            label: 'Github',
+          },
+          {
+            href: 'http://whitepaper.helium.com',
+            label: 'Whitepaper (2018)',
+            position: 'right',
+          },
+          {
+            href: 'https://explorer.helium.com',
+            label: 'Explorer',
+            position: 'right',
+          },
+          {
+            href: 'https://network.helium.com/coverage',
+            label: 'Coverage Map',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} Helium Foundation`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+
   markdown: {
     mermaid: true,
   },
+
   themes: ['@docusaurus/theme-mermaid'],
 }
+
+module.exports = config
