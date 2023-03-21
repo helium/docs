74bcdd80fb601a926642ae2dfc5d74fa5c09f577
Joey Hiller <1965053+jthiller@users.noreply.github.com>
2023-03-21 14:37:01 -0700

    mobile network section

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -70,7 +70,7 @@ module.exports = {
           position: 'right',
         },
         {
-          to: '/',
+          to: '5g-on-helium',
           label: 'Mobile Network',
           position: 'right',
         },
