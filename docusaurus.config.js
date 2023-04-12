e8c72ae8d2de840d68f1d2ffe0baadefd1f63b0f
Chewing Glass <chewinglass123@gmail.com>
2023-04-12 15:15:29 -0500

    Use devnet

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -130,7 +130,7 @@ module.exports = {
   ],
   customFields: {
     MIGRATION_SERVICE_URL: 'https://migration.web.helium.io',
-    SOLANA_URL: 'solana-rpc.web.helium.io/?session-key=Pluto',
+    SOLANA_URL: 'solana-rpc.web.test-helium.com/?session-key=Pluto',
   },
   plugins: [
     function (context, options) {
