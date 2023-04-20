4b8b479a1972d895753bffa6e983552430a1d751
Chewing Glass <chewinglass123@gmail.com>
2023-04-20 09:07:44 -0500

    Try signAndSend

--- src/theme/HntToFeeSol.tsx
+++ src/theme/HntToFeeSol.tsx
@@ -2,15 +2,16 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import { useConnection, useWallet } from '@solana/wallet-adapter-react'
 import axios from 'axios'
 import React, { useState } from 'react'
-import { useAsyncCallback } from 'react-async-hook'
+import { useAsync, useAsyncCallback } from 'react-async-hook'
 import './bufferFill'
 import { Alert, AlertIcon } from './components/Alert'
 import { Button } from './components/Button'
 import { Flex } from './components/Flex'
 import { Wallet, WrapWithAccountProvider } from './components/Wallet'
 import { Transaction } from '@solana/web3.js'
-import { sendAndConfirmWithRetry } from '@helium/spl-utils'
+import { sendAndConfirmWithRetry, toNumber } from '@helium/spl-utils'
 import { Checkbox } from './components/Checkbox'
+import { BN } from '@coral-xyz/anchor'
 
 // Default styles that can be overridden by your app
 require('@solana/wallet-adapter-react-ui/styles.css')
@@ -20,21 +21,31 @@ export const HntToFeeSolImpl = () => {
   const url = siteConfig.customFields.HNT_TO_RENT_SERVICE_URL
   const { publicKey, wallet } = useWallet()
   const { connection } = useConnection()
+  const { result: estimate } = useAsync(async () => {
+    const { estimate } = (
+      await axios.post(`${url}/estimate`, {
+        wallet: publicKey.toBase58(),
+      })
+    ).data
+
+    return toNumber(new BN(estimate), 8)
+  }, [])
+
   const { execute, error, loading } = useAsyncCallback(async () => {
     if (wallet && connection && wallet.adapter && wallet.adapter.connected) {
       const txRaw = (await axios.post(`${url}/hnt-to-fees`, {
         wallet: publicKey.toBase58()
       })).data
       const tx = Transaction.from(Buffer.from(txRaw))
-      const signed = await wallet.adapter.signTransaction(tx)
-      await sendAndConfirmWithRetry(
-        connection,
-        signed.serialize(),
-        {
-          skipPreflight: true,
-        },
-        'confirmed',
-      )
+      await wallet.adapter.signAndSendTransaction(tx)
+      // await sendAndConfirmWithRetry(
+      //   connection,
+      //   signed.serialize(),
+      //   {
+      //     skipPreflight: true,
+      //   },
+      //   'confirmed',
+      // )
     }
   })
   const [isChecked, setIsChecked] = useState(false);
@@ -52,7 +63,7 @@ export const HntToFeeSolImpl = () => {
       uses of this system. I further agree that I will not use this system for any illegal purpose.
       <Checkbox onChange={() => setIsChecked((c) => !c)}>I Agree</Checkbox>
       <Button isDisabled={!isChecked} colorScheme="primary" isLoading={loading} onClick={execute}>
-        {loading ? 'Loading' : 'Convert HNT to 0.02 SOL for tx fees'}
+        {loading ? 'Loading' : `Convert ${estimate} HNT to 0.02 SOL for tx fee`}
       </Button>
     </Flex>
   )
