8055b6626214875929e4a2639aed29ba88e56c02
Joey Hiller <1965053+jthiller@users.noreply.github.com>
2023-05-22 15:20:45 -0700

    Establish Sphere walkthrough (#1333)
    * Establish Sphere walkthrough
    
    Co-Authored-By: lytzV <44751312+lytzV@users.noreply.github.com>
    
    * Optimize images
    
    * image optimizer run
    
    * reference links
    
    * url slug shortened
    
    * link edits
    
    ---------
    
    Co-authored-by: lytzV <44751312+lytzV@users.noreply.github.com>
    Co-authored-by: Sam Gutentag <1404219+samgutentag@users.noreply.github.com>

--- sidebarsDocs.js
+++ sidebarsDocs.js
@@ -68,7 +68,13 @@ module.exports = {
       items: ['helium-tokens/hnt-token', 'helium-tokens/iot-token', 'helium-tokens/mobile-token'],
       collapsed: true,
     },
-    'helium-tokens/data-credit',
+    {
+      type: 'category',
+      label: 'Data Credit Tokens',
+      link: { type: 'doc', id: 'helium-tokens/data-credit' },
+      items: ['helium-tokens/data-credit', 'helium-tokens/data-credit-portal'],
+      collapsed: true,
+    },
     {
       type: 'category',
       label: 'Vote Escrow Tokens',
