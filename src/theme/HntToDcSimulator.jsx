93fdc09f2fc13b3d81e3c6f078a92384d2b8dd53
Joey Hiller <1965053+jthiller@users.noreply.github.com>
2023-11-25 19:11:47 -0800

    Add HNT to DC simulation widget

---
+++ src/theme/HntToDcSimulator.jsx
@@ -0,0 +1,130 @@
+import React, { useState, useEffect } from "react";
+import { Connection, PublicKey } from "@solana/web3.js";
+import { PythConnection } from "@pythnetwork/client";
+
+export const HntToDcSimulator = () => {
+  const [liveHntPrice, setLiveHntPrice] = useState(0);
+  const [simulatedHntPrice, setSimulatedHntPrice] = useState(1);
+  const [dcAmount, setDcAmount] = useState(100000);
+  const [sliderRange, setSliderRange] = useState({ min: 0, max: 5 });
+  const [isInitialPriceSet, setIsInitialPriceSet] = useState(false);
+
+  useEffect(() => {
+    const connection = new Connection("https://api.devnet.solana.com");
+    const programKey = new PublicKey("gSbePebfvPy7tRqimPoVecS2UsBvYv46ynrzWocc92s");
+
+    const pythConnection = new PythConnection(connection, programKey);
+    pythConnection.onPriceChange((product, price) => {
+      if (product.symbol === "Crypto.HNT/USD" && price.price) {
+        setLiveHntPrice(price.price);
+        if (!isInitialPriceSet) {
+          setInitialSliderValues(price.price);
+          setIsInitialPriceSet(true);
+        }
+      }
+    });
+
+    pythConnection.start();
+    return () => pythConnection.stop();
+  }, [isInitialPriceSet]);
+
+  const setInitialSliderValues = (price) => {
+    // based on queried price, update the initial position and min/max of the input
+    const roundedPrice = Math.round(price * 100) / 100;
+    setSimulatedHntPrice(roundedPrice);
+    setDcAmount(calculateDc(roundedPrice));
+    const upperLimit = Math.ceil(price / 5) * 5 + 5;
+    let lowerLimit = Math.max(0, Math.floor(price / 5) * 5);
+    if (lowerLimit == 0) {
+      lowerLimit = 0.01;
+    }
+    setSliderRange({ min: lowerLimit, max: upperLimit });
+  };
+
+  const calculateDc = (price) => {
+    const DC_PRICE = 0.00001;
+    return price / DC_PRICE;
+  };
+
+  const handleSliderChange = (event) => {
+    const newPrice = parseFloat(event.target.value);
+    setSimulatedHntPrice(newPrice);
+    setDcAmount(calculateDc(newPrice));
+  };
+
+  const handleSetToLivePrice = () => {
+    const roundedPrice = Math.round(liveHntPrice * 100) / 100;
+    setSimulatedHntPrice(roundedPrice);
+    setDcAmount(calculateDc(roundedPrice));
+  };
+
+  const hntForTenDollars = 10 / simulatedHntPrice;
+
+  return (
+    <div style={{ padding: "16px 16px 0" }}>
+      <table className="dc-calc-table">
+        <thead>
+          <tr>
+            <th>DC from 1 HNT</th>
+            <th>HNT burned for $10 DC</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{dcAmount.toLocaleString()} DC</td>
+            <td>{hntForTenDollars.toLocaleString()} HNT</td>
+          </tr>
+        </tbody>
+      </table>
+      <div
+        style={{
+          background: "white",
+          padding: "16px 16px 0",
+          marginBottom: "1em",
+        }}
+        className="dc-calc-table"
+      >
+        <div style={{ display: "flex", padding: "0.125em 0 0.125em 0" }}>
+          <input
+            type="range"
+            min={sliderRange.min}
+            max={sliderRange.max}
+            step="0.01"
+            value={simulatedHntPrice}
+            onChange={handleSliderChange}
+            style={{ flex: "1 1 auto" }}
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "baseline",
+          }}
+        >
+          <p>Simulated HNT Oracle Price: ${simulatedHntPrice.toFixed(2)}</p>
+          {liveHntPrice ? (
+            <button
+              onClick={handleSetToLivePrice}
+              style={{
+                appearance: "none",
+                background: "transparent",
+                border: "none",
+                cursor: "pointer",
+                color: "#53627c",
+                fontSize: "1.125rem",
+                lineHeight: "1.65rem",
+                textAlign: "right",
+                fontVariantNumeric: "tabular-nums",
+              }}
+            >
+              Live Oracle Price: ${liveHntPrice.toFixed(6)}
+            </button>
+          ) : (
+            ""
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
