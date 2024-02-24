8b3508605ecb71cdffda9267e8fe6455850475a4
Victor Li <lytz@Victors-MacBook-Pro.local>
2024-02-24 14:19:59 -0800

    buy and fund oui page updates

--- docs/network-iot/run-an-lns/FundAnOUI.jsx
+++ docs/network-iot/run-an-lns/FundAnOUI.jsx
@@ -1,13 +1,13 @@
 export const OUIUsage = () => (
   <figure className="screensnippet-wrapper">
     <iframe
-      src="https://dune.com/embeds/2608923/4325514"
+      src="https://dune.com/embeds/3463955/5822522/"
       frameborder="0"
       width="100%"
       height="350px"
     ></iframe>
     <figcaption>
-      Up-to-date usage metrics for all OUIs on the Helium network. <br /> To get a sense of traffic
+      Up-to-date usage metrics for all IoT OUIs on the Helium network. <br /> To get a sense of traffic
       and DC usage on the network, check out the full{' '}
       <a href="https://dune.com/helium-foundation/helium-data-credits">
         Dune Data Credit Dashboard
