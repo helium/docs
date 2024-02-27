72e957b1dd67f79223372266aafa39c75f3db43e
Joey Hiller <1965053+jthiller@users.noreply.github.com>
2024-02-26 19:07:40 -0800

    Prettier

--- src/theme/TopLedgerEmbed.tsx
+++ src/theme/TopLedgerEmbed.tsx
@@ -11,19 +11,21 @@ export const TopLedgerEmbed = ({
   apiKey,
   params = {},
 }) => {
-
-  const embedUrl = new URL(`https://analytics.topledger.xyz/${client}/embed/query/${queryId}/visualization/${visualizationId}`)
-  embedUrl.search = new URLSearchParams({ api_key: apiKey, hide_header: 'true', hide_timestamp: 'true', iframe: 'true', ...params }).toString()
+  const embedUrl = new URL(
+    `https://analytics.topledger.xyz/${client}/embed/query/${queryId}/visualization/${visualizationId}`,
+  )
+  embedUrl.search = new URLSearchParams({
+    api_key: apiKey,
+    hide_header: 'true',
+    hide_timestamp: 'true',
+    iframe: 'true',
+    ...params,
+  }).toString()
 
   return (
-    <figure
-      className={`screensnippet-wrapper ${styles.tlembedWrapper}`}
-    >
+    <figure className={`screensnippet-wrapper ${styles.tlembedWrapper}`}>
       {title && <p>{title}</p>}
-      <iframe
-        src={embedUrl.toString()}
-        className={styles.tlembed}
-      ></iframe>
+      <iframe src={embedUrl.toString()} className={styles.tlembed}></iframe>
       {caption && <figcaption>{caption}</figcaption>}
     </figure>
   )
