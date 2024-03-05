65f1e4e2a284e7446cf4d9ccaca6c69eb3aaa5fe
Joey Hiller <1965053+jthiller@users.noreply.github.com>
2024-03-04 22:13:43 -0800

    General styling

--- src/theme/Spoiler.tsx
+++ src/theme/Spoiler.tsx
@@ -1,36 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 export const Spoiler = ({ children }) => {
-  const [revealed, setRevealed] = useState(false);
+  const [revealed, setRevealed] = useState(false)
 
-  
   const styles = {
-    obscured: {
-      backgroundColor: 'gray',
+    containerRevealed: {
       cursor: 'pointer',
     },
-    revealed: {
-      backgroundColor: 'transparent',
+    containerObscured: {
+      backgroundColor: '#f5f7fd',
       cursor: 'pointer',
+      position: 'relative',
+      borderRadius: '24px',
+      overflow: 'hidden',
     },
+    textVisible: {},
     textHidden: {
-      visibility: 'hidden',
+      //keep text rendered on screen for screenreaders
+      filter: 'blur(4px)',
+      opacity: '0.2',
     },
-    textVisible: {
-      visibility: 'visible',
+    clickableNoticeActive: {
+      backgroundColor: 'white',
+      position: 'absolute',
+      left: '50%',
+      padding: '0.25em 1em',
+      transform: 'translateX(-50%) translateY(50%)',
+      borderRadius: '100px',
+      border: '1px solid #e4e4ee',
+      zIndex: '1',
     },
-  };
+    clickableNoticeInactive: {
+      display: 'none',
+    },
+  }
 
-  const containerStyle = revealed ? styles.revealed : styles.obscured;
-  const textStyle = revealed ? styles.textVisible : styles.textHidden;
+  const containerStyle = revealed ? styles.containerRevealed : styles.containerObscured
+  const textStyle = revealed ? styles.textVisible : styles.textHidden
+  const clickableNotice = revealed ? styles.clickableNoticeInactive : styles.clickableNoticeActive
 
   return (
-    <div
-      style={containerStyle}
-      onClick={() => setRevealed(!revealed)}
-    >
-      <span style={clickableNotice}>Spoiler: </span>
+    <div style={containerStyle} onClick={() => setRevealed(!revealed)}>
+      <span style={clickableNotice}>Reveal</span>
       <span style={textStyle}>{children}</span>
     </div>
-  );
-};
+  )
+}
