346987806f6531785db9b230c57f01c7a86f9d89
jpad-freedomfi <61293007+jpad-freedomfi@users.noreply.github.com>
2025-10-30 22:42:31 -0600

    Cisco-catalyst-9800-converted-guide
    Cisco-catalyst-9800-converted-guide

--- sidebarsDocs.js
+++ sidebarsDocs.js
@@ -30,6 +30,7 @@ module.exports = {
             'network-mobile/data-only-guides/data-only-aruba',
             'network-mobile/data-only-guides/data-only-juniper-mist',
             'network-mobile/data-only-guides/data-only-meraki',
+            'network-mobile/data-only-guides/data-only-Cisco-WLC-9800',
             'network-mobile/data-only-guides/data-only-mikrotik',
             'network-mobile/data-only-guides/data-only-ruckus',
             'network-mobile/data-only-guides/data-only-ubiquiti',
